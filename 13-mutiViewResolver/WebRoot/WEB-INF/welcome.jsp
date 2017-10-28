61aec90e9ed2b58a5f9503e543325a4a49a23331
xurui <742744538@qq.com>
2017-10-28 11:44:48 +0800

    mutilViewResolver -- commit

---
+++ 13-mutiViewResolver/WebRoot/WEB-INF/welcome.jsp
@@ -0,0 +1,13 @@
+<%@ page language="java" import="java.util.*" pageEncoding="utf-8"%>
+
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+  <head>
+    <title>welcome</title>
+  </head>
+  
+  <body>
+    <h3>hello world!!!!!!!!</h3>
+  </body>
+</html>
---
+++ 13-mutiViewResolver/src/com/xurui/handler/MyController.java
@@ -0,0 +1,18 @@
+package com.xurui.handler;
+import javax.servlet.http.HttpServletRequest;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+
+public class MyController implements Controller {
+			
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+		return new ModelAndView("e-commerce");
+		//return new ModelAndView("internal");
+	}
+}
